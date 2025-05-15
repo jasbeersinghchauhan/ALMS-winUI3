3ef315ba7338704d94d1e86fa4b01d06d8979253
Jasbeer Singh Chauhan <jasbeersinghchauhan377@gmail.com>
2025-05-16 00:16:06 +0530

    Prepared for UI integration

--- librarian.h
+++ librarian.h
@@ -14,22 +14,28 @@
 class librarian {
 public:
     // Account-related functions
-    void viewAccountById(sql::Connection* con, int accountId, std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount);
-    void viewAccountByEmail(sql::Connection* con, const std::string& email, std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount);
-    void viewAllAccounts(sql::Connection* con, std::function<void(Account)> displayAccount);
+    void viewAccountById(int accountId, std::function<void(std::string)> showMessage,
+        std::function<void(Account)> displayAccount);
+    void viewAccountByEmail(const std::string& email, std::function<void(std::string)> showMessage,
+        std::function<void(Account)> displayAccount);
+    void viewAllAccounts(std::function<void(Account)> displayAccount, std::function<void(std::string)> showMessage);
 
     // Book-related functions
-    void addBook(sql::Connection* con, const Book& newBook);
-    void updateBook(sql::Connection* con, const Book& updatedBook);
-    void deleteBook(sql::Connection* con, const std::string& isbn);
-    void viewAllBooks(sql::Connection* con, std::function<void(Book)> displayBook);
-    void viewAvailableBooks(sql::Connection* con, std::vector<Book>& outBooks);
+    void addBook(const std::string& title, const std::string& author,
+        const std::string& isbn, int year, int quantity,
+        std::function<void(std::string)> showMessage);
+
+    void updateBook(const std::string& isbn, const std::string& title, const std::string& author,
+        int year, int quantity, std::function<void(std::string)> showMessage);
+    void deleteBook(const std::string& isbn, std::function<void(std::string)> showMessage);
+    void viewAllBooks(std::function<void(Book)> displayBook, std::function<void(std::string)> showMessage);
+    static void viewAvailableBooks(std::function<void(Book)> displayBook, std::function<void(std::string)> showMessage);
 
     // Borrowing-related functions
-    void viewAllBorrowedBooks(sql::Connection* con, std::vector<BorrowedBook>& outBorrowedBooks);
-    void viewBorrowedBooksByAccountId(sql::Connection* con, int accountId, std::vector<BorrowedBook>& outBorrowedBooks);
-    bool allocateBook(sql::Connection* con, int accountId, int bookId);
-    bool revertBookAllocation(sql::Connection* con, int accountId, int bookId);
+    void viewAllBorrowedBooks(std::function<void(BorrowedBook)> displayBorrowedBook, std::function<void(std::string)> showMessage);
+    void viewBorrowedBooksByAccountId(int accountId, std::function<void(BorrowedBook)> displayBorrowedBook, std::function<void(std::string)> showMessage);
+    void allocateBook(int accountId, int bookId, std::function<void(std::string)> showMessage, std::function<void(bool)> onResult);
+    void revertBookAllocation(int accountId, int bookId, std::function<void(std::string)> showMessage, std::function<void(bool)> onResult);
 };
 
 #endif
