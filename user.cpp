6b99fbce7f697748e234b4762ce08a367c1c3b34
Jasbeer Singh Chauhan <jasbeersinghchauhan377@gmail.com>
2025-05-16 00:12:35 +0530

    Prepared for UI integration

--- user.cpp
+++ user.cpp
@@ -1,86 +1,123 @@
-﻿#include "user.h"
+#include "pch.h"
+#include "user.h"
+#include "dataBaseService.h"
+
 #include <sstream>
 #include <ctime>
 #include <cstdio>
+#include <iomanip>
 
 User::User(int id) : accountId(id) {}
 
-std::string User::getUserDetails(sql::Connection* con) {
-    Account account(accountId);
-    account.searchAccountByID(con);
-
-    std::ostringstream details;
-    details << "Account ID: " << account.getAccountId() << "\n";
-    details << "Name: " << account.getName() << "\n";
-    details << "Email: " << account.getEmail() << "\n";
-    details << "Phone: " << account.getPhone() << "\n";
-    return details.str();
+void User::getUserDetails(std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount) {
+    DatabaseService::withConnection(showMessage, [&](sql::Connection* con) {
+        Account account(accountId);
+        std::string resultMessage = account.searchAccountByID(con);
+        showMessage(resultMessage);
+        if (account.getAccountId() != 0) {
+            displayAccount(account);
+        }
+    });
 }
 
-std::vector<BorrowedBook> User::getBorrowedBooks(sql::Connection* con) {
-    BorrowedBooksManager manager(con);
-    return manager.getBorrowedBooksByUserID(accountId);
+void User::getBorrowedBooks(std::function<void(BorrowedBook)> displayBorrowedBook, std::function<void(std::string)> showMessage) {
+    DatabaseService::withConnection(showMessage, [&](sql::Connection* con) {
+        BorrowedBooksManager manager(con);
+        auto borrowed = manager.getBorrowedBooksByUserID(accountId, showMessage);
+        for (const auto& book : borrowed) {
+            displayBorrowedBook(book);
+        }
+        });
 }
 
-std::vector<BorrowedBook> User::getOverdueBooks(sql::Connection* con) {
-    BorrowedBooksManager manager(con);
-    std::vector<BorrowedBook> borrowed = manager.getBorrowedBooksByUserID(accountId);
-    std::vector<BorrowedBook> overdue;
+void User::getOverdueBooks(std::function<void(BorrowedBook)> displayOverdueBook, std::function<void(std::string)> showMessage) {
+    DatabaseService::withConnection(showMessage, [&](sql::Connection* con) {
+        BorrowedBooksManager manager(con);
+        auto borrowed = manager.getBorrowedBooksByUserID(accountId, showMessage);
 
-    std::time_t now = std::time(nullptr);
+        std::time_t now = std::time(nullptr);
+        constexpr int OVERDUE_DAYS = 14;
 
-    for (const auto& book : borrowed) {
-        if (!book.returned) {
-            std::tm borrowTm = {};
-            int y, m, d;
-            if (std::sscanf(book.borrowDate.c_str(), "%d-%d-%d", &y, &m, &d) == 3) {
-                borrowTm.tm_year = y - 1900;
-                borrowTm.tm_mon = m - 1;
-                borrowTm.tm_mday = d;
+        for (const auto& book : borrowed) {
+            if (!book.returned) {
+                std::tm borrowTm{};
+                std::istringstream iss(book.borrowDate);
+                iss >> std::get_time(&borrowTm, "%Y-%m-%d");
 
-                std::time_t borrowTime = std::mktime(&borrowTm);
-                double days = std::difftime(now, borrowTime) / (60 * 60 * 24);
+                if (!iss.fail()) {
+                    std::time_t borrowTime = std::mktime(&borrowTm);
+                    double days = std::difftime(now, borrowTime) / (60 * 60 * 24);
 
-                if (days > 14) {
-                    overdue.push_back(book);
+                    if (days > OVERDUE_DAYS) {
+                        displayOverdueBook(book);
+                    }
+                }
+                else {
+                    if (showMessage) {
+                        showMessage("Failed to parse borrow date: " + book.borrowDate);
+                    }
                 }
             }
         }
-    }
-
-    return overdue;
+        });
 }
 
-double User::getTotalFine(sql::Connection* con) {
-    BorrowedBooksManager manager(con);
-    std::vector<BorrowedBook> borrowed = manager.getBorrowedBooksByUserID(accountId);
-    double totalFine = 0.0;
-    std::time_t now = std::time(nullptr);
 
-    for (const auto& book : borrowed) {
-        if (!book.returned) {
-            std::tm borrowTm = {};
-            int y, m, d;
-            if (std::sscanf(book.borrowDate.c_str(), "%d-%d-%d", &y, &m, &d) == 3) {
-                borrowTm.tm_year = y - 1900;
-                borrowTm.tm_mon = m - 1;
-                borrowTm.tm_mday = d;
+void User::getTotalFine(std::function<void(double)> displayTotalFine, std::function<void(std::string)> showMessage) {
+    DatabaseService::withConnection(showMessage, [&](sql::Connection* con) {
+        BorrowedBooksManager manager(con);
+        auto borrowed = manager.getBorrowedBooksByUserID(accountId, showMessage);
 
-                std::time_t borrowTime = std::mktime(&borrowTm);
-                double days = std::difftime(now, borrowTime) / (60 * 60 * 24);
+        std::time_t now = std::time(nullptr);
+        constexpr int ALLOWED_DAYS = 14;
+        constexpr double FINE_PER_DAY = 2.0;
+        double totalFine = 0.0;
 
-                int overdueDays = static_cast<int>(days) - 14;
-                if (overdueDays > 0) {
-                    totalFine += overdueDays * 2.0;
+        for (const auto& book : borrowed) {
+            if (!book.returned) {
+                std::tm borrowTm{};
+                std::istringstream iss(book.borrowDate);
+                iss >> std::get_time(&borrowTm, "%Y-%m-%d");
+
+                if (!iss.fail()) {
+                    std::time_t borrowTime = std::mktime(&borrowTm);
+                    double days = std::difftime(now, borrowTime) / (60 * 60 * 24);
+
+                    int overdueDays = static_cast<int>(days) - ALLOWED_DAYS;
+                    if (overdueDays > 0) {
+                        totalFine += overdueDays * FINE_PER_DAY;
+                    }
+                }
+                else {
+                    if (showMessage) {
+                        showMessage("Failed to parse borrow date: " + book.borrowDate);
+                    }
                 }
             }
         }
-    }
 
-    return totalFine;
+        displayTotalFine(totalFine);
+        });
 }
 
-std::vector<Book> User::getAvailableBooks(sql::Connection* con) {
-    Book book;
-    return book.getAvailableBooks(con);
+
+void User::getAvailableBooks(std::function<void(Book)> displayBook, std::function<void(std::string)> showMessage) {
+    DatabaseService::withConnection(showMessage, [&](sql::Connection* con) {
+        Book book;
+        std::string error;
+        auto books = book.getAvailableBooks(con, error);
+
+        if (!error.empty()) {
+            showMessage("Error fetching books: " + error);
+            return;
+        }
+        if (books.empty()) {
+            showMessage("No books found.");
+        }
+        else {
+            for (const auto& b : books) {
+                displayBook(b);
+            }
+        }
+        });
 }
