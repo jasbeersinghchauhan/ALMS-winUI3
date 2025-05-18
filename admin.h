504bf5e7f259a9890da7f7a9d68452a898fa7459
Jasbeer Singh Chauhan <jasbeersinghchauhan377@gmail.com>
2025-05-19 00:51:45 +0530

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <pch.h>
 #include "account.h"
 #include "book.h"
 #include "borrowedBooks.h"
@@ -12,12 +11,9 @@ class Admin {
 public:
     Admin() = default;
 
+    //Account Management
     void addAccount(const std::string& name, const std::string& email, const std::string& password, const std::string& phone,
         const std::string& role, std::function<void(std::string)> showMessage);
-
-    void viewAccountById(int accountId, std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount);
-
-    void viewAccountByEmail(const std::string& email, std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount);
 
     void updateAccount(int accountId, const std::string& name, const std::string& email, const std::string& phone, const std::string& role,
         std::function<void(std::string)> showMessage);
@@ -26,9 +22,15 @@ public:
 
     void deleteAccount(int accountId, std::function<void(std::string)> showMessage);
 
+    void viewAccountById(int accountId, std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount);
+
+    void viewAccountByEmail(const std::string& email, std::function<void(std::string)> showMessage, std::function<void(Account)> displayAccount);
+
     void viewAllAccounts(std::function<void(Account)> displayAccount, std::function<void(std::string)> showMessage);
 
-    void addBook(const std::string& title, const std::string& author,const std::string& isbn, int year, int quantity,
+
+    //Book Management
+    void addBook(const std::string& title, const std::string& author, const std::string& isbn, int year, int quantity,
         std::function<void(std::string)> showMessage);
 
     void updateBook(const std::string& isbn, const std::string& title, const std::string& author, int year, int quantity, 
@@ -38,8 +40,17 @@ public:
 
     void viewAllBooks(std::function<void(Book)> displayBook, std::function<void(std::string)> showMessage);
 
+    std::vector<Book> searchBookByTitle(const std::string& title, std::function<void(std::string)> showMessage);
+
+    std::vector<Book> searchBookByISBN(const std::string& isbn, std::function<void(std::string)> showMessage);
+
+    std::vector<Book> searchBookByAuthor(const std::string& author, std::function<void(std::string)> showMessage);
+
+    //Borrowed Books Management
     void viewAllBorrowedBooks(std::function<void(BorrowedBook)> displayBorrowedBook, std::function<void(std::string)> showMessage);
 
     void viewBorrowedBooksByAccountId(int accountId, std::function<void(BorrowedBook)> displayBorrowedBook, 
         std::function<void(std::string)> showMessage);
+
+    //Delete borrowed book record
 };
