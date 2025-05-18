5fe2b5b7dce00bad8961cb9bd5b4457c40f33527
Jasbeer Singh Chauhan <jasbeersinghchauhan377@gmail.com>
2025-05-19 00:52:27 +0530

    Update librarian.h

--- librarian.h
+++ librarian.h
@@ -18,24 +18,23 @@ public:
         std::function<void(Account)> displayAccount);
     void viewAccountByEmail(const std::string& email, std::function<void(std::string)> showMessage,
         std::function<void(Account)> displayAccount);
-    void viewAllAccounts(std::function<void(Account)> displayAccount, std::function<void(std::string)> showMessage);
 
     // Book-related functions
-    void addBook(const std::string& title, const std::string& author,
-        const std::string& isbn, int year, int quantity,
+    void addBook(const std::string& title, const std::string& author, const std::string& isbn, int year, int quantity,
         std::function<void(std::string)> showMessage);
 
-    void updateBook(const std::string& isbn, const std::string& title, const std::string& author,
-        int year, int quantity, std::function<void(std::string)> showMessage);
+    void updateBook(const std::string& isbn, const std::string& title, const std::string& author, int year, int quantity, 
+        std::function<void(std::string)> showMessage);
     void deleteBook(const std::string& isbn, std::function<void(std::string)> showMessage);
-    void viewAllBooks(std::function<void(Book)> displayBook, std::function<void(std::string)> showMessage);
     static void viewAvailableBooks(std::function<void(Book)> displayBook, std::function<void(std::string)> showMessage);
+    // Search Book by title and isbn
 
     // Borrowing-related functions
     void viewAllBorrowedBooks(std::function<void(BorrowedBook)> displayBorrowedBook, std::function<void(std::string)> showMessage);
     void viewBorrowedBooksByAccountId(int accountId, std::function<void(BorrowedBook)> displayBorrowedBook, std::function<void(std::string)> showMessage);
     void allocateBook(int accountId, int bookId, std::function<void(std::string)> showMessage, std::function<void(bool)> onResult);
     void revertBookAllocation(int accountId, int bookId, std::function<void(std::string)> showMessage, std::function<void(bool)> onResult);
+    // Return book
 };
 
 #endif
