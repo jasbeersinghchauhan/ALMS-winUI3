1108e51437988fe35a4b7ffb0620e824121bb9b4
Jasbeer Singh Chauhan <jasbeersinghchauhan377@gmail.com>
2025-06-13 01:12:26 +0530

    Create login.h

---
+++ login.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <functional>
+
+class Login {
+private:
+	std::string userID;
+	std::string password;
+public:
+	Login(const std::string& user, const std::string& pass);
+
+	std::string authenticate(std::function<void(std::string)> showMessage) {};
+};
